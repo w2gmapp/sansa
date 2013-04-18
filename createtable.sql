d45397baf396f0f2c1cf8145c74f280e567d7fcf
w2gmapp <w2gmapp@gmail.com>
2013-04-18 15:33:39 -0300

    Create createtable.sql
    This file will create a table in the database to house the users information.

---
+++ createtable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (  
+`UserID` INT(25) NOT NULL AUTO_INCREMENT PRIMARY KEY ,  
+`Username` VARCHAR(65) NOT NULL ,  
+`Password` VARCHAR(32) NOT NULL ,  
+`EmailAddress` VARCHAR(255) NOT NULL  
+); 
